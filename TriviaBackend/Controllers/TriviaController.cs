e2a0492636a3fab8e7e196c50b97e0dbab6fe1e8
Jeffrey DeVince <jdevince@users.noreply.github.com>
2020-04-14 19:12:42 -0500

    initial signalr proof of concept working

---
+++ TriviaBackend/Controllers/TriviaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TriviaBackend.Hubs;
+
+namespace TriviaBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TriviaController : ControllerBase
+    {
+        private readonly ILogger<TriviaController> _logger;
+        private TriviaHub _triviaHub;
+
+        public TriviaController(ILogger<TriviaController> logger, TriviaHub triviaHub)
+        {
+            _logger = logger;
+            _triviaHub = triviaHub;
+        }
+
+        [HttpPost("start-game")]
+        public bool StartGame()
+        {
+            _triviaHub.BroadcastMessage("name", "mes");
+            return true;
+        }
+    }
+}
